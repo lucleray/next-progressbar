4274ed3c9445f1c6ba12baeaeab39f3c68ca5010
luc <luc.leray@gmail.com>
2018-04-24 22:23:41 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const WebpackBar = require('webpackbar')
+
+const dev = process.env.NODE_ENV !== 'production'
+
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+        )
+      }
+
+      const { dev, isServer } = options
+
+      config.plugins.push(
+        new WebpackBar({
+          name: isServer ? 'server' : 'client',
+          color: isServer ? 'orange' : 'green'
+        })
+      )
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+## next-progressbar
+
+[Next.js](https://github.com/zeit/next.js) + [WebpackBar](https://github.com/nuxt/webpackbar)
+
+### Installation
+
+```
+yarn add next-progressbar
+```
+
+### Usage
+
+Edit your next config :
+
+```js
+// next.config.js
+const withProgressBar = require('next-progressbar')
+
+module.exports = withProgressBar({
+  // rest of your next config
+})
+```
